1ab9f8ceee497848f12f7afe00c955cd379b2eb8
alumno <alumno@vm1>
2020-11-19 08:36:11 +0100

    Solución total al ejercicio definicion de las rutas

--- routes/web.php
+++ routes/web.php
@@ -24,3 +24,23 @@ Route::get('/', function () {
 Route::get('login', function () {
     return 'Login usuario';
 });
+
+Route::get('logout', function () {
+    return 'Logout usuario';
+});
+
+Route::get('catalog', function () {
+    return 'Listado peliculas';
+});
+
+Route::get('catalog/show/{id}', function ($id) { 
+    return 'Vista detalle película ' . $id;
+});
+
+Route::get('catalog/create', function () {
+    return 'Añadir pelicula';
+});
+
+Route::get('catalog/edit/{id}', function ($id) {
+    return 'Modificar pelicula ' . $id;
+});
