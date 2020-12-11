a39b6ed9c84fa653da414ce1438f1c7d895c01e9
Jose Ramon Lopez <alumno@vm1>
2020-12-11 09:00:57 +0100

    Solucion completa a ejerciciosIV

--- app/Http/Controllers/CatalogController.php
+++ app/Http/Controllers/CatalogController.php
@@ -31,4 +31,32 @@ class CatalogController extends Controller
     	return view('catalog.edit', array('pelicula' => Movie::findOrFail($id)));
     }
 
+    public function putEdit(Request $request){
+        $id = $request->input('id');
+        $pelicula = Movie::findOrFail($id); //la recuperamos a traves del modelo pasando el id
+
+        //Modificamos con los datos del formulario
+        $pelicula->title = $request->input('title');
+		$pelicula->year = $request->input('year');
+		$pelicula->director = $request->input('director');
+		$pelicula->poster = $request->input('poster');
+		$pelicula->synopsis = $request->input('synopsis');
+        $pelicula->save();
+        //Redirigimos al show
+        return redirect('/catalog/show/'.$id);
+    }
+
+    public function postCreate(Request $request){
+        $pelicula = new Movie();
+        $pelicula->title = $request->input('title');
+		$pelicula->year = $request->input('year');
+		$pelicula->director = $request->input('director');
+		$pelicula->poster = $request->input('poster');
+		$pelicula->synopsis = $request->input('synopsis');
+		$pelicula->save();
+        return redirect('/catalog');
+    }
+
+
+
 }
